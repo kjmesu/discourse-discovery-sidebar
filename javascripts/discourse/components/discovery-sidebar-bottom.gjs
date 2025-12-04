9465dfefd022bec8da40c191744b83db1bd3a9df
kjm <kjmesu@proton.me>
2025-12-04 22:07:00 +0000

    refactor(discovery-sidebar): Improve bottom sidebar positioning

--- javascripts/discourse/components/discovery-sidebar-bottom.gjs
+++ javascripts/discourse/components/discovery-sidebar-bottom.gjs
@@ -1,8 +1,9 @@
 import Component from "@glimmer/component";
+import positionFixedToColumn from "../modifiers/position-fixed-to-column";
 
 export default class DiscoverySidebarBottom extends Component {
   <template>
-    <aside class="discovery-sidebar-bottom">
+    <aside class="discovery-sidebar-bottom" {{positionFixedToColumn}}>
       <div class="discovery-sidebar__content">
         <h3>Bottom Sidebar</h3>
         <p>This sidebar sticks to the bottom of the viewport.</p>
---
+++ javascripts/discourse/modifiers/position-fixed-to-column.js
@@ -0,0 +1,30 @@
+import { registerDestructor } from "@ember/destroyable";
+import Modifier from "ember-modifier";
+
+export default class PositionFixedToColumn extends Modifier {
+  constructor(owner, args) {
+    super(owner, args);
+    registerDestructor(this, (instance) => instance.cleanup());
+  }
+
+  modify(element) {
+    this.element = element;
+    this.updatePosition();
+
+    window.addEventListener("resize", this.updatePosition);
+    window.addEventListener("scroll", this.updatePosition);
+  }
+
+  updatePosition = () => {
+    const column = this.element?.closest(".discovery-sidebar-column");
+    if (column) {
+      const rect = column.getBoundingClientRect();
+      this.element.style.left = `${rect.left}px`;
+    }
+  };
+
+  cleanup() {
+    window.removeEventListener("resize", this.updatePosition);
+    window.removeEventListener("scroll", this.updatePosition);
+  }
+}
